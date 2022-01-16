594b81b1dce30fd2d469d6b3740b9abce75f733f
Joan Tasayco <joan.tasayco@outlook.com>
2022-01-16 07:46:43 -0500

    feat: update in reporte-cucumber.js

--- reporte-cucumber.js
+++ reporte-cucumber.js
@@ -6,22 +6,22 @@ report.generate({
 	metadata:{
         browser: {
             name: 'chrome',
-            version: '60'
+            version: '69'
         },
-        device: 'Local test machine',
+        device: 'PC Local',
         platform: {
-            name: 'ubuntu',
-            version: '16.04'
+            name: 'windows',
+            version: '10'
         }
     },
     customData: {
         title: 'Run info',
         data: [
-            {label: 'Project', value: 'Custom project'},
-            {label: 'Release', value: '1.2.3'},
-            {label: 'Cycle', value: 'B11221.34321'},
-            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+            {label: 'Project', value: 'Test Automation'},
+            {label: 'Release', value: '1'},
+            {label: 'Cycle', value: '1'},
+            {label: 'Execution Start Time', value: 'Enero 2022, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'Enero 2022, 02:56 PM EST'}
         ]
     }
 });
